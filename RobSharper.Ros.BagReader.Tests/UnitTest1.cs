62e968b41ea2abe9024f59083c36a6f252330d49
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2020-12-07 15:24:00 +0100

    Projects created

---
+++ RobSharper.Ros.BagReader.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace RobSharper.Ros.BagReader.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}

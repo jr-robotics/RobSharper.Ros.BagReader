3e5c8734458e30320f2cd1f88213d6cc6a45bbc1
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2020-12-09 10:06:47 +0100

    Removed duplicate code

--- RobSharper.Ros.BagReader/Records/RecordHeaderValue.cs
+++ RobSharper.Ros.BagReader/Records/RecordHeaderValue.cs
@@ -1,11 +1,13 @@
 using System;
+using System.IO;
 using System.Text;
+using RobSharper.Ros.MessageEssentials.Serialization;
 
 namespace RobSharper.Ros.BagReader.Records
 {
     public class RecordHeaderValue
     {
-        private readonly Lazy<byte[]> _reversedData;
+        private readonly Lazy<byte[]> _littleEndianData;
         private readonly byte[] _data;
 
         public byte[] Data => _data;
@@ -13,17 +15,7 @@ namespace RobSharper.Ros.BagReader.Records
         public RecordHeaderValue(byte[] data)
         {
             _data = data ?? throw new ArgumentNullException(nameof(data));
-            _reversedData = new Lazy<byte[]>(() =>
-            {
-                var d = _data;
-                
-                if (!BitConverter.IsLittleEndian)
-                {
-                    Array.Reverse(d);
-                }
-
-                return d;
-            });
+            _littleEndianData = new Lazy<byte[]>(() => _data.ToLittleEndian());
         }
 
         public string ConvertToString()
@@ -44,7 +36,7 @@ namespace RobSharper.Ros.BagReader.Records
             if (Data.Length != 4)
                 throw new InvalidCastException();
             
-            return BitConverter.ToInt32(_reversedData.Value, 0);
+            return BitConverter.ToInt32(_littleEndianData.Value, 0);
         }
 
         public long ConvertToInt64()
@@ -52,13 +44,13 @@ namespace RobSharper.Ros.BagReader.Records
             if (Data.Length != 8)
                 throw new InvalidCastException();
             
-            return BitConverter.ToInt64(_reversedData.Value, 0);
+            return BitConverter.ToInt64(_littleEndianData.Value, 0);
         }
 
         public DateTime ConvertToDateTime()
         {
-            var secs = BitConverter.ToInt32(_reversedData.Value, 0);
-            var nsecs = BitConverter.ToInt32(_reversedData.Value, 4);
+            var secs = BitConverter.ToInt32(_littleEndianData.Value, 0);
+            var nsecs = BitConverter.ToInt32(_littleEndianData.Value, 4);
 
             var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)
                 .AddSeconds(secs)

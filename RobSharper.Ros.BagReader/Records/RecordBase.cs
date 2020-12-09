2f105d9d216d00d50ba597c639f149bf717ac685
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2020-12-09 14:17:11 +0100

    Moved code to separate file

---
+++ RobSharper.Ros.BagReader/Records/RecordBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using RobSharper.Ros.MessageEssentials.Serialization;
+
+namespace RobSharper.Ros.BagReader.Records
+{
+    public abstract class RecordBase : IBagRecord
+    {
+        private readonly RosBinaryReader _recordData;
+        private bool _disposed;
+
+        public RecordBase(RosBinaryReader recordData)
+        {
+            _recordData = recordData;
+        }
+
+        public abstract void Accept(IBagRecordVisitor visitor);
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing && _recordData != null)
+            {
+                var bytesToSkip = _recordData.BaseStream.Length - _recordData.BaseStream.Position;
+                _recordData.SkipBytes((int)bytesToSkip);
+                _recordData?.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
